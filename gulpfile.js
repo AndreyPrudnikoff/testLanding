2ce1b6ca90e452ba3a4086eea87d2ad0f3ca19db
AndrewPru <58787232+AndrewPru@users.noreply.github.com>
2020-09-16 00:56:31 +0300

    version 1.1

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+const scss = require('gulp-sass');
+const cleanCss = require('gulp-clean-css');
+const rename = require('gulp-rename');
+
+gulp.task('compile-scss', () => {
+    return gulp.src('./styles/*.scss')
+        .pipe(scss())
+        .pipe(cleanCss())
+        .pipe(rename('style.css'))
+        .pipe(gulp.dest('./styles/'));
+});
+
+gulp.task('watcher', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("*.html").on("change", reload);
+    gulp.watch("./styles/*.scss").on("change", reload);
+    gulp.watch('./styles/*.scss', gulp.series('compile-scss'), reload);
+});
